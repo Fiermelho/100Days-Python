c21cbad81f07d1f1592d5c8c5797ab5078fa34e6
Fiermelho <59830853+Fiermelho@users.noreply.github.com>
2023-03-20 17:18:23 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,13 @@
-#steamlit
+import pandas as pd
+import streamlit as st
+
+st.title("Hi Amber! Welcome to Streamlit!")
+
+st.write("My first DataFrame")
+
+st.write(
+pd.DataFrame({
+    'A': [1, 5, 9, 7],
+    'B': [3, 2, 4, 8]
+  })
+)
